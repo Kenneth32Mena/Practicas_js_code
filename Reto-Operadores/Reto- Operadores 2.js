21e99b2312f7ab1a6e3278d9a35a18d12a8ad4ff
Kenneth32Mena <kenneth2024@hotmail.com>
2026-02-13 08:54:59 -0600

    Actualizacion de Reto Operadores 2

---
+++ Reto-Operadores/Reto- Operadores 2.js
@@ -0,0 +1,14 @@
+var obj1 = { propieda: 'valor' };
+var obj2 = { prodad: 'simp' };
+console.log(`El numero 10 es mayor que el numero 5 ? ${10 > 5}`);
+console.log(`El numero 10 es menor que el numero 5? ${10 < 5}`);
+console.log(`El numero 1 es igual al numero 2? ${1 == 2}`);
+console.log(`El texto 1 es igual al texto 2  ${'texto1' == 'texto2'}`);
+console.log(`Los objetos son iguales ${obj1 === obj2}`);
+console.log('AND logico: false');
+console.log('OR logico: true');
+console.log(`La suma de 10+3: ${10 + 3}`);
+console.log(`La resta de 10 -3: ${10 - 3}`)
+console.log(`La multiplicacion de 10*3: ${10 * 3}`);
+console.log(`La division de 10/3: ${(10 / 3)}`);
+console.log(`10 modulo de 3: ${10 % 3}`);
