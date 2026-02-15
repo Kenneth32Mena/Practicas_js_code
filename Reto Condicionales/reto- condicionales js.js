0072514bcc06a94e3d06c2dad9ab3b3330bd9dcf
Kenneth32Mena <kenneth2024@hotmail.com>
2026-02-15 13:20:09 -0600

    Add files via upload

---
+++ Reto Condicionales/reto- condicionales js.js
@@ -0,0 +1,46 @@
+/*
+let edad =60;
+if (edad>0 && edad<=100){
+  console.log("Edad valida");
+  if (edad>0 && edad<=5){
+    console.log("Eres un bebé");
+  }
+  else if (edad>5 && edad<=12 ){
+    console.log("Eres un niño");
+  }
+  else if (edad>12 && edad<=17){
+    console.log("Eres un adolecente");
+  }
+  else if (edad>17 && edad<=25){
+    console.log("Eres un adulto Joven");
+  }
+  else if (edad>25 && edad<60){
+    console.log("Eres un adulto");
+  }
+  else if (edad>=60){
+    console.log("Eres un adulto mayor");
+  }
+}
+else{
+  console.log("Edad no valida");
+}
+*/
+/*
+let accion='';
+switch (accion){
+  case 'crear':
+  console.log('La accion es crear');
+  break;
+  case 'colocar':
+  console.log('La accion es colocar');
+  break;
+  case 'actualizar':
+  console.log('La accion es actualizar');
+  break;
+  case 'borrar':
+  console.log('La accion es borrar');
+}
+*/
+let num = 2;
+let resultado = (num % 2 === 0) ? "Es par " : 'Es impar'
+console.log(`El numero ingresado es ${resultado}`)
