ce14aa4b7eb00b318d8645a8197cf878d51c35c5
Kenneth32Mena <kenneth2024@hotmail.com>
2026-02-22 22:06:45 -0600

    Practicas del dia de hoy

---
+++ Practicas usando CLASS/practica.js
@@ -0,0 +1,44 @@
+/*class Alumno {
+  constructor(nombre,grado,materiaFavorita) {
+    this.nombre=nombre;
+    this.grado=grado;
+    this.materiaFavorita=materiaFavorita;
+}
+presentarse() {
+  console.log(`soy ${this.nombre}, estoy en ${this.grado} grado y mi materia favorita es ${this.materiaFavorita}`);
+}
+estudiar(){
+  console.log(`${this.nombre} esta estudiando ${this.materiaFavorita}`);
+}
+}
+let a1= new Alumno('Kenneth', 12, 'Computacion');
+let a2= new Alumno('Juan', 5, 'English');
+let a3= new Alumno('Pedro', 12, 'Educacion Fisica');
+a1.presentarse();
+a1.estudiar();
+a2.presentarse();
+a2.estudiar();
+a3.presentarse();
+a3.estudiar();
+*/
+/*
+class Vehiculo{
+  constructor(marca,modelo,año){
+    this.marca=marca;
+    this.modelo=modelo;
+    this.año=año;
+  }
+  infoVehiculos(){
+    console.log(this.marca+" "+this.modelo+" "+this.año);
+  }
+  edadVehiculos(){
+    const year= 2026;
+    let yearActual=year-this.año;
+    console.log(`La edad de este vehiculo desde que salio de agencia es de ${yearActual} años`);
+  }
+}
+let carro1= new Vehiculo('Bajaj', 'Boxer 100BM ES' , 2022);
+carro1.infoVehiculos();
+carro1.edadVehiculos();
+*/
+
